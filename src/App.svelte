1a5439decf93dd9ce1024b107b10162d5de4b556
Channaveera <nimbagalkori@gmail.com>
2021-08-07 16:40:09 +0530

    sevelte with basic routing and tailwind css

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Router, Link, Route } from "yrv";
+	import Home from "./home.svelte";
+
+  
+	export let url = "";
+  </script>
+  
+  <Router url="{url}">
+	
+	  <Link to="/">
+		<Link to="/user"></Link>
+	</Link>
+	  
+	 
+	
+	
+	  <Route path="/"><Home /></Route>
+	
+  </Router>
